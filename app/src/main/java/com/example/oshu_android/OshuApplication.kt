3750e017b9bc23f0587680e99fd669be5c53815b
ashxom <anseha090115@naver.com>
2026-07-14 08:50:22 +0900

    feat :: 카카오맵 초기화 클래스와 위치 권한 등록

---
+++ app/src/main/java/com/example/oshu_android/OshuApplication.kt
@@ -0,0 +1,18 @@
+package com.example.oshu_android
+
+import android.app.Application
+import com.kakao.vectormap.KakaoMapSdk
+
+class OshuApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoMapSdk.init(
+            this,
+            getString(
+                R.string.kakao_native_app_key
+            ),
+        )
+    }
+}
