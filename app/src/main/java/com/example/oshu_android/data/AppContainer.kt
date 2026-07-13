bb9f7244b84a31439013cda7ecc4c03068103378
ashxom <anseha090115@naver.com>
2026-07-13 20:12:12 +0900

    feat :: 로그인 의존성 관리를 위한 AppContainer 추가

---
+++ app/src/main/java/com/example/oshu_android/data/AppContainer.kt
@@ -0,0 +1,34 @@
+package com.example.oshu_android.data
+
+import android.content.Context
+import com.example.oshu_android.data.auth.AuthModule
+import com.example.oshu_android.data.auth.LoginRepository
+import com.example.oshu_android.data.auth.LoginRepositoryImpl
+import com.example.oshu_android.data.auth.LoginResponse
+import com.example.oshu_android.data.auth.SessionStore
+
+class AppContainer(
+    context: Context,
+) {
+    private val sessionStore: SessionStore =
+        MemorySessionStore()
+
+    val loginRepository: LoginRepository =
+        LoginRepositoryImpl(
+            authApi = AuthModule.provideAuthApi(
+                context.applicationContext
+            ),
+            sessionStore = sessionStore,
+        )
+}
+
+private class MemorySessionStore : SessionStore {
+    private var currentSession: LoginResponse? = null
+
+    override suspend fun save(
+        response: LoginResponse,
+        persist: Boolean,
+    ) {
+        currentSession = response
+    }
+}
