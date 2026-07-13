17d59c6d68b1248fa19d0005d124c01ebac12999
ashxom <anseha090115@naver.com>
2026-07-13 19:30:53 +0900

    refactor :: OSHU 색상 팔레트를 MaterialTheme으로 통합

--- app/src/main/java/com/example/oshu_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/oshu_android/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.oshu_android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val OshuPink = Color(0xFFFF829B)
+val OshuPinkLight = Color(0xFFFFF7F8)
+val OshuBorder = Color(0xFFD8D8D8)
+val OshuHint = Color(0xFF999999)
+val OshuError = Color(0xFFFF3B30)
+val OshuTextPrimary = Color(0xFF222222)
+val OshuTextSecondary = Color(0xFF666666)
+val OshuWhite = Color(0xFFFFFFFF)
