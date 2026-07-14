860adc465c77fc4a2db28d25238deb46aa157fa3
ashxom <anseha090115@naver.com>
2026-07-14 11:32:12 +0900

    fix :: 잘못된 코드 수정

--- app/src/main/java/com/example/oshu_android/OshuApplication.kt
+++ app/src/main/java/com/example/oshu_android/OshuApplication.kt
@@ -4,15 +4,12 @@ import android.app.Application
 import com.kakao.vectormap.KakaoMapSdk
 
 class OshuApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
 
         KakaoMapSdk.init(
             this,
-            getString(
-                R.string.kakao_native_app_key
-            )
+            getString(R.string.kakao_native_app_key)
         )
     }
 }
